6ca0c7e0f32349ae42755d757c8506eb6f73c911
Joe Klesczewski <jklesczewski@gmail.com>
2019-09-29 00:26:45 -0400

    Can now paint the texture (brick sphere).
    The painting may or may not be saving to disk. We want it not to be. This needs more testing.

--- Assets/ClickToColor.cs
+++ Assets/ClickToColor.cs
@@ -4,37 +4,30 @@ using UnityEngine;
 
 public class ClickToColor : MonoBehaviour
 {
-    public Material[] materials;
     public Renderer rend;
     private int index = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rend = GetComponent<Renderer>();
-        //rend.enabled = true;
-        //materials = GetComponent<Material[]>();
-        //materials.enabled = true;
+
     }
     public void OnMouseDown()
     {
-        //if (materials.Length == 0)
-        //{
-        //    return;
-        //}
         if (Input.GetMouseButtonDown(0))
         {
-            //            index += 1;
-            //            if (index == materials.Length + 1)
-            //            {
-            //                index = 1;
-            //            }
-            //            print(index);
-            //            rend.sharedMaterial = materials[index - 1];
+            // Cycle through colors when clicked
             if(rend.material.GetColor("_Color") == Color.red)       { rend.material.SetColor("_Color", Color.blue); }
             else if(rend.material.GetColor("_Color") == Color.blue) { rend.material.SetColor("_Color", Color.green); }
             else                                                    { rend.material.SetColor("_Color", Color.red); }
-            
+
+            // Get clicked spot
+            Vector3 clickedPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Texture2D tex = rend.material.mainTexture as Texture2D;
+            //print(tex.dimension.ToString());
+            print(clickedPoint);
+            //print(tex.GetPixel(x, y));
         }
     }
 
---
+++ Assets/ClickToModifyTexture.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToModifyTexture : MonoBehaviour
+{
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        if (!Input.GetMouseButton(0)) { return; }
+
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            print("Out 1");
+            return;
+        }
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+        {
+            print((rend == null).ToString() + " " + (rend.sharedMaterial == null).ToString() + " " + (rend.sharedMaterial.mainTexture == null).ToString() + " " + (meshCollider == null).ToString());
+            return;
+        }
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        //print(pixelUV.x.ToString() + ", " + pixelUV.y.ToString());
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+        print(pixelUV.x.ToString() + ", " + pixelUV.y.ToString());
+
+        int patchW = (int) 0.1 * tex.width;
+        int patchH = (int) 0.1 * tex.height;
+
+        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.black);
+        //for(int xp = -1*patchW; xp < patchW; ++xp)
+        for(int xp = 50; xp < 100; ++xp)
+        {
+            //for(int yp = -1*patchH; yp < patchH; ++yp)
+            for (int yp = -50; yp < 100; ++yp)
+            {
+                tex.SetPixel(((int)pixelUV.x + xp) % tex.width, ((int)pixelUV.y + yp) % tex.height, Color.black);
+            }
+        }
+        
+
+        tex.Apply();
+
+        print(tex.GetPixel((int)pixelUV.x, (int)pixelUV.y).ToString());
+    }
+}
